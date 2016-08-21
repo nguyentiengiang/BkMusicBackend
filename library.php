a826cc229dc7ff4713c9c5c1c8a3eec33e89ad8f
Giang Nguyen Tien <nguyentiengiang@outlook.com>
2016-08-22 02:21:23 +0700

    init

---
+++ library.php
@@ -0,0 +1,13 @@
+<?php
+
+define('ROOT', __DIR__);
+require(ROOT. '/vendor/autoload.php');
+
+$app = new Slim\App();
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    $response->write("Hello, " . $args['name']);
+    return $response;
+});
+
+$app->run();
