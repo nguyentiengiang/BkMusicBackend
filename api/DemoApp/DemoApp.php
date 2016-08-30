cbeb408bf9c073416f723de2171612dc9f47754e
Giang Nguyen Tien <nguyentiengiang@outlook.com>
2016-08-30 10:36:53 +0700

    Add DemoApp for Template

---
+++ api/DemoApp/DemoApp.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Demo Application
+ */
+
+class Demo extends s10Core\defaultApp {
+    
+    /**
+     * Re-define public properties in __construct() method (except $logMessage for WRITE TO LOG)
+     * 
+     */
+    public function __construct() {
+        self::$isWriteLog = TRUE;
+        self::$isDebug = DEBUG_MODE;
+        self::$appName = get_class($this);
+        self::$arrDatabaseConfig = [];
+        self::$arrSlimContainer = [];
+        
+        // Write magic method __construct() of parent(defaultApp) after re-define public properties
+        parent::__construct();
+    }
+    
+    /**
+     * define request methods here
+     * 
+     */
+    public function enableMethods() {
+        $this->app->get('/{name}', self::$appName . '::index');
+    }
+        
+    public function index($request, $response) {
+        $name = $request->getAttribute('name');
+        $response->getBody()->write("Hello, $name");
+        return $response;
+    }
+        
+}
---
+++ api/DemoApp/index.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Define debug mode for apis application
+ * set FALSE to turn off debug
+ */
+//define("DEBUG_MODE", FALSE);
+define("DEBUG_MODE", TRUE);
+
+/**
+ * REQUIRE for run APIs
+ * DO NOT Touch this file if must needed
+ */
+define('ROOT', __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+require(ROOT . 'config' . DIRECTORY_SEPARATOR . 'bootstrapSlimAPI.php');
+
+/**
+ * 
+ * Define name of api application choose what ever you want
+ */
+$classApplication = 'DemoApp';
+require_once($classApplication . '.php');
+
+/**
+ * Function call to start Slim APIs application
+ * 
+ * @return void
+ */
+
+function startApp($classApplication) {
+    new $classApplication();
+}
+
+/*
+ * Run Slim app in mode defined
+ */
+if (!DEBUG_MODE) {
+    startApp($classApplication);
+} else {
+    $bench = new Ubench;
+    $bench->start();
+    startApp($classApplication);
+    $bench->end();
+    $str = PHP_EOL . 'Time: ' . $bench->getTime(true) . ' microsecond -> ' . $bench->getTime(false, '%d%s');
+    $str .= PHP_EOL . 'MemoryPeak: ' . $bench->getMemoryPeak(true) . ' bytes -> ' . $bench->getMemoryPeak(false, '%.3f%s');
+    $str .= PHP_EOL . 'MemoryUsage: ' . $bench->getMemoryUsage(true);
+    s10Core\MyFile\Log::write($str, $classApplication, "TestBenchmark" . $classApplication);
+    unset($str);
+}
