4d243b30af9a72be9c4888069aab87962a20d331
Giang Nguyen Tien <nguyentiengiang@outlook.com>
2016-10-09 03:27:01 +0700

    fix error reporting

--- api/BkMusic/index.php
+++ api/BkMusic/index.php
@@ -18,9 +18,8 @@ require(ROOT . 'config' . DIRECTORY_SEPARATOR . 'bootstrapSlimApi.php');
 /**
  * SET SHOW ERROR
  */
-//ini_set('display_startup_errors',1);
-//ini_set('display_errors',1);
-//error_reporting(-1);
+//error_reporting(0);
+error_reporting(E_ERROR);
 
 /**
  * 
